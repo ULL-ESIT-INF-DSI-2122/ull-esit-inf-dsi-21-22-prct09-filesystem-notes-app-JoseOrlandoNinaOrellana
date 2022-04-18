95cc93b35ea6cbbfe83edc18acc193cfd59fa6fd
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-04-18 09:52:43 +0000

    añadir node.yml action

--- src/Reduce.ts
+++ src/Reduce.ts
@@ -1,14 +1,33 @@
+/**
+ * Clase Reduce que emula el reduce de un array
+ */
 export abstract class Reduce {
+    /**
+     * Constructor
+     * @param array Array de números
+     * @param result Resultado del reduce
+     */
     constructor(protected array: number[], protected result: number = 0) {}
 
+    /**
+     * Método abstracto reduce
+     */
     protected abstract reduce(): number
 
+    /**
+     * Método abstracto printresult
+     * @returns Mensaje indicando el resultado del reduce
+     */
     protected abstract printResult(): string
 
     protected init() {}
 
     protected afterReduce() {}
 
+    /**
+     * Métod run
+     * @returns Mensaje con el resultado
+     */
     public run(): string {
         this.init();
         this.reduce();
